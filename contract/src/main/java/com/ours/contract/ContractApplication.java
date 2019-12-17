6725a3637c41c7f4336c26d9d9565b6168aaca31
yinyisi <2533793744@qq.com>
2019-12-17 15:29:45 +0800

    合同和web两个模块添加注解

--- contract/src/main/java/com/ours/contract/ContractApplication.java
+++ contract/src/main/java/com/ours/contract/ContractApplication.java
@@ -2,8 +2,14 @@ package com.ours.contract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.session.FlushMode;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRedisHttpSession(flushMode = FlushMode.IMMEDIATE, maxInactiveIntervalInSeconds = 1800)
 public class ContractApplication {
 
     public static void main(String[] args) {
